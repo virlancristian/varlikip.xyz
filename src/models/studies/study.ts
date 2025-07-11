72d5d9ec2e8fb6c6eca8a380e954d9d6f6375ec9
virlancristian <rethvol111@gmail.com>
2025-07-11 14:43:40 +0300

    feat: studies page

---
+++ src/models/studies/study.ts
@@ -0,0 +1,9 @@
+export interface Study {
+    degree: string;
+    faculty: string;
+    university: string;
+    startDate: string;
+    endDate: string;
+    description: string;
+    courses: string[];
+}
--- src/pages/studies.tsx
+++ src/pages/studies.tsx
@@ -1,7 +1,21 @@
 import { JSX } from "react";
 
+import { Study } from "../models/studies/study";
+import { StudyContainer } from "../components/studies/study.container";
+
+import studiesData from "../data/studies.json";
+import "../css/gradients.css";
+
 export function StudiesPage(): JSX.Element {
+    const studies: Study[] = studiesData as Study[];
+
     return (
-        <div>Studies</div>
+        <div>
+            {
+                studies.map((study: Study, idx: number) => (
+                    <StudyContainer study={study} containerBackgroundClassnames={[idx % 2 === 0 ? "blue-white-gradient" : "black-white-gradient"]}/>
+                ))
+            }
+        </div>
     );
 }
