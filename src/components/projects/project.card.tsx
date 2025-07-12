a8c22fc103c2090dd5ec0478d22aa4dbd5aef035
virlancristian <rethvol111@gmail.com>
2025-07-12 21:35:26 +0300

    feat: projects page

--- src/components/projects/project.card.tsx
+++ src/components/projects/project.card.tsx
@@ -1,11 +1,24 @@
 import { JSX } from "react";
-import { Project } from "../../models/project/project";
 import classNames from "classnames";
+
+import { Project } from "../../models/project/project";
 
 interface Props {
     project: Project;
     hidden: boolean;
 }
+
+const listElementCSSClasses: string[] = [
+    "font-clash-grotesk-light",
+    "text-lg",
+    "list-disc",
+    "w-5/6",
+    "my-1"
+];
+
+const linkCSSClasses: string[] = [
+    "text-blue-500"
+];
 
 export function ProjectCard(props: Props): JSX.Element {
     const {
@@ -14,22 +27,22 @@ export function ProjectCard(props: Props): JSX.Element {
     } = props;
 
     return (
-        <div className={classNames(hidden ? "hidden" : "", "text-white", "carousel-card")}>
-            <h1>{project.title}</h1>
-            <ul>
-                <li>{project.description}</li>
-                <li>Techonolgies used: {project.technologies.join(", ")}</li>
+        <div className={classNames(hidden ? "hidden" : "", "text-white carousel-card bg-gray-800 bg-opacity-60 project-card rounded-xl flex flex-col items-center overflow-hidden")}>
+            <h1 className="font-clash-grotesk-semibold text-lg my-2 w-5/6 text-center">{project.title}</h1>
+            <ul className="flex flex-col items-center">
+                <li className={classNames(...listElementCSSClasses)}>{project.description}</li>
+                <li className={classNames(...listElementCSSClasses)}>Techonolgies used: {project.technologies.join(", ")}</li>
                 {
                     project.source !== "" && (
-                        <li>
-                            <a href={project.source}>Source code</a>
+                        <li className={classNames(...listElementCSSClasses)}>
+                            <a href={project.source} className={classNames(...linkCSSClasses)}>Source code</a>
                         </li>
                     )
                 }
                 {
                     project.url !== "" && (
-                        <li>
-                            <a href={project.url}>Check out the application!</a>
+                        <li className={classNames(...listElementCSSClasses)}>
+                            <a href={project.url} className={classNames(...linkCSSClasses)}>Check out the application!</a>
                         </li>
                     )
                 }
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -6,7 +6,7 @@ import "../css/projects.css";
 
 export function ProjectsPage(): JSX.Element {
     return (
-        <div className="project-slider-container blue-white-gradient">
+        <div className="project-slider-container blue-white-gradient flex justify-center items-center">
             <ProjectSlider/>
         </div>
     );
